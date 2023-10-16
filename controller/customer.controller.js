d44e6c7c1f7ad7d30b099cfe97c1d48aca0f82ce
prajwalnarola <128449514+prajwalnarola@users.noreply.github.com>
2023-10-16 10:28:12 +0530

    applied validation to retrieve data from database

--- controller/customer.controller.js
+++ controller/customer.controller.js
@@ -10,15 +10,26 @@ function createCustomer(req, res) {
   if (!req.body.name || !req.body.email || !req.body.age) {
     return res.status(400).send({ status: 0, data: "Bad Request" });
   }
-  Customer.create(customerData)
-    .then((data) => {
-      res
-        .status(200)
-        .send({ status: 1, message: "customer created successfully", data });
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  Customer.findOne({ where: { email: req.body.email } }).then((customer) => {
+    if (!customer) {
+      Customer.create(customerData)
+        .then((data) => {
+          res.status(200).send({
+            status: 1,
+            message: "customer created successfully",
+            data,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+    } else {
+      res.status(404).send({
+        status: 0,
+        message: "Customer already present with same email: " + req.body.email,
+      });
+    }
+  });
 }
 
 function getCustomers(req, res) {
@@ -34,27 +45,51 @@ function getCustomers(req, res) {
 }
 
 function getCustomerById(req, res) {
-  Customer.findByPk(req.query.id)
-    .then((data) => {
-      res
-        .status(200)
-        .send({ status: 1, message: "customer data available for id: "+req.query.id, data });
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  const customerId = req.query.id;
+
+  Customer.findOne({ where: { id: customerId } }).then((customer) => {
+    if (customer) {
+      Customer.findByPk(req.query.id)
+        .then((data) => {
+          res.status(200).send({
+            status: 1,
+            message: "customer data available for id: " + req.query.id,
+            data,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+    } else {
+      res.status(404).send({
+        status: 0,
+        message: "Customer not found for id: " + customerId,
+      });
+    }
+  });
 }
 
 function getCustomerByEmail(req, res) {
-  Customer.findOne({ where: { email: req.query.email } })
-    .then((data) => {
-      res
-        .status(200)
-        .send({ status: 1, message: "customer data available for email: "+req.query.email, data });
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
+  Customer.findOne({ where: { email: req.query.email } }).then((customer) => {
+    if (customer) {
+      Customer.findOne({ where: { email: req.query.email } })
+        .then((data) => {
+          res.status(200).send({
+            status: 1,
+            message: "customer data available for email: " + req.query.email,
+            data,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+    } else {
+      res.status(404).send({
+        status: 0,
+        message: "Customer not found for email: " + req.query.email,
+      });
+    }
+  });
 }
 
 function updateCustomer(req, res) {
@@ -63,48 +98,56 @@ function updateCustomer(req, res) {
     email: req.body.email,
     age: req.body.age,
   };
-  Customer.update(newCustomerData, { where: { email: req.body.email } })
-    .then(() => {
-      res.status(200).send({
-        status: 1,
-        message: "customer updated successfully for email: " + req.body.email,
+
+  Customer.findOne({ where: { email: req.body.email } }).then((customer) => {
+    if (customer) {
+      Customer.update(newCustomerData, { where: { email: req.body.email } })
+        .then(() => {
+          res.status(200).send({
+            status: 1,
+            message:
+              "customer updated successfully for email: " + req.body.email,
+          });
+        })
+        .catch((error) => {
+          res.status(500).send(error);
+        });
+    } else {
+      res.status(404).send({
+        status: 0,
+        message: "Customer not found for email: " + req.body.email,
       });
+    }
+  });
+}
+
+function deleteCustomer(req, res) {
+  const customerId = req.query.id;
+
+  Customer.findOne({ where: { id: customerId } })
+    .then((customer) => {
+      if (customer) {
+        Customer.destroy({ where: { id: customerId } })
+          .then(() => {
+            res.status(200).send({
+              status: 1,
+              message: "Customer deleted successfully for id: " + customerId,
+            });
+          })
+          .catch((error) => {
+            res.status(500).send(error);
+          });
+      } else {
+        res.status(404).send({
+          status: 0,
+          message: "Customer not found for id: " + customerId,
+        });
+      }
     })
     .catch((error) => {
       res.status(500).send(error);
     });
 }
-
-function deleteCustomer(req, res) {
-    const customerId = req.query.id;
-  
-    Customer.findOne({ where: { id: customerId } })
-      .then((customer) => {
-        if (customer) {
-          // Customer with the specified ID exists, so you can proceed with deletion.
-          Customer.destroy({ where: { id: customerId } })
-            .then(() => {
-              res.status(200).send({
-                status: 1,
-                message: "Customer deleted successfully for id: " + customerId,
-              });
-            })
-            .catch((error) => {
-              res.status(500).send(error);
-            });
-        } else {
-          // Customer with the specified ID doesn't exist.
-          res.status(404).send({
-            status: 0,
-            message: "Customer not found for id: " + customerId,
-          });
-        }
-      })
-      .catch((error) => {
-        res.status(500).send(error);
-      });
-  }
-  
 
 module.exports = {
   createCustomer,
